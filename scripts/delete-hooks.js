d4d0d2ee183036f6fe8377c29d5d85929696dd36
Mihir Chaturvedi <plibither8@gmail.com>
2021-01-24 23:00:01 +0530

    Fix: modify delete-hook script

--- scripts/delete-hooks.js
+++ scripts/delete-hooks.js
@@ -2,15 +2,15 @@ const fetch = require('node-fetch')
 const { ghAuthHeaders, getPublicRepos } = require('../utils')
 
 async function main() {
+  const hookUrl = 'https://git.mihir.ch/webhook/push'
   const repos = await getPublicRepos()
   let count = 0
   for (const repo of repos) {
-    const hooks = await fetch(repo.hookUrl, { headers: ghAuthHeaders })
-      .then(res => res.json())
-    const id = hooks.find(({ config }) => config.url === 'https://git.mihir.ch/webhook/push')?.id
-    if (id) {
+    const hooks = await fetch(repo.hookUrl, { headers: ghAuthHeaders }).then(res => res.json())
+    const hook = hooks.find(({ config }) => config.url === hookUrl)
+    if (hook?.id) {
       console.log(`[${++count}/${repos.length}] Deleting hook for ${repo.name}`)
-      await fetch(repo.hookUrl + '/' + id, {
+      await fetch(repo.hookUrl + '/' + hook.id, {
         method: 'DELETE',
         headers: ghAuthHeaders
       })
