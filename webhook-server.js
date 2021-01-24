69eecce2db290bf2322227eb36b8608078cae350
Mihir Chaturvedi <plibither8@gmail.com>
2021-01-24 16:03:15 +0530

    Fix: Add push webhook handler

--- webhook-server.js
+++ webhook-server.js
@@ -1,9 +1,18 @@
 require('dotenv').config()
 const polka = require('polka')
+const { json } = require('body-parser')
+const config = require('./config.json')
+const git = require('simple-git')()
+
+async function pull(repo) {
+  const localPath = `${config.repositoriesPath}/${repoPath}`
+  await git.cwd(localPath).pull('origin', 'master')
+}
 
 polka()
-  .use((req, res, next) => {
-    console.log(req.url)
-    res.end(JSON.stringify(req.url))
+  .use(json())
+  .post('/webhook/push', async (req, res) => {
+    const { repository } = req.body
+    pull(repository.full_name)
   })
   .listen(process.env.HOOKS_PORT)
