7c9aa309d698c5c14663d4d2ba49432418f5059b
Mihir Chaturvedi <plibither8@gmail.com>
2021-03-18 20:09:54 +0530

    Fix: Explicitly provide .env path to work using cron

--- utils.js
+++ utils.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require('dotenv').config({ path: __dirname + '/.env' })
 const { writeFile } = require('fs/promises')
 const path = require('path')
 const fetch = require('node-fetch')
